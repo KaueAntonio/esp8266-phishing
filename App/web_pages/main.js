622cfcee607a43ae26be3f4e54f348c760b1069b
KaueAntonio <kaue4991@gmail.com>
2023-09-29 10:15:30 -0300

    Refactor: Correcting Identation

--- App/web_pages/main.js
+++ App/web_pages/main.js
@@ -1,16 +1,16 @@
-function getFile(adr, callback, timeout, method, onTimeout, onError){
-	if(adr === undefined) return;
-	if(callback === undefined) callback = function(){};
-	if(timeout === undefined) timeout = 8000;
-	if(method === undefined) method = "GET";
-	if(onTimeout === undefined) {
-		onTimeout = function(){
-			console.log("ERROR: timeout loading file "+adr);
+function getFile(adr, callback, timeout, method, onTimeout, onError) {
+	if (adr === undefined) return;
+	if (callback === undefined) callback = function () { };
+	if (timeout === undefined) timeout = 8000;
+	if (method === undefined) method = "GET";
+	if (onTimeout === undefined) {
+		onTimeout = function () {
+			console.log("ERROR: timeout loading file " + adr);
 		};
 	}
-	if(onError === undefined){
-		onError = function(){
-			console.log("ERROR: loading file: "+adr);
+	if (onError === undefined) {
+		onError = function () {
+			console.log("ERROR: loading file: " + adr);
 		};
 	}
 
@@ -20,11 +20,11 @@ function getFile(adr, callback, timeout, method, onTimeout, onError){
 	request.open(method, encodeURI(adr), true);
 	request.timeout = timeout;
 	request.ontimeout = onTimeout;
-  request.onerror = onError;
+	request.onerror = onError;
 	request.overrideMimeType("application/json");
-	request.onreadystatechange = function() {
-		if(this.readyState == 4){
-			if(this.status == 200){
+	request.onreadystatechange = function () {
+		if (this.readyState == 4) {
+			if (this.status == 200) {
 				callback(this.responseText);
 			}
 		}
